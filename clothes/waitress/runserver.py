e730490678b43f6409a76dd764a0b89c251d00ac
josephine bonka <josephinebonka123@gmail.com>
2021-05-24 14:08:59 +0300

    migration

---
+++ clothes/waitress/runserver.py
@@ -0,0 +1,4 @@
+from waitress import server
+from python_project.wsgi import application
+
+if __name__"" '_main_'
--- python_project/wsgi.py
+++ python_project/wsgi.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 import os
 
-
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'python_project.settings')
 
+application = get_wsgi_application()
